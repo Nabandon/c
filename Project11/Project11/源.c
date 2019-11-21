dfa019568138c3c9b21212452a4fad6533c695e4
Nabandon <1839304120@qq.com>
2019-11-21 20:46:14 +0800

    接收一个整形数值,按顺序打印每一位

--- Project11/Project11/源.c
+++ Project11/Project11/源.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+int Binrayseacher(int size, int arr[], int tofind)
+{
+	int left = 0;
+	int right = size - 1;
+	while (left <= right)
+	{
+		int mid = (left+right)/2;
+		if (arr[mid] < tofind)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid]>tofind)
+		{
+			right = mid - 1;
+		}
+		else { return mid;
+		}
+	}
+}
 int main()
 {
-	int letter = 0;
-	printf("ÊäÈëÒ»¸ö×Ö·û:\n");
-		letter = getchar();
-		if (letter >= 'a'&&letter <= 'z')
-		{
-			putchar(letter - 32);
-		}
-		else if (letter >= 'A'&&letter <= 'Z')
-		{
-			putchar(letter + 32);
-		}
-		if (0 <= letter <= 9)
-		printf("\n");
-		system("pause");
-		return 0;
+	int arr[6] = { 1, 2, 3, 4, 5, 6 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+	int ret = Binrayseacher(size, arr, 5);
+		printf("ret=%d\n",ret);
+		system("pause");		
 }
