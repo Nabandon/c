c7abcf900e7da4ce066de2d45e94fa690887fb4d
Nabandon <1839304120@qq.com>
2019-12-04 20:36:43 +0800

    条件运算

---
+++ Project56/Project56/源.cpp
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdlib.h >
+int max(int x,int y)
+{
+	return (x > y ? x : y)+100;
+}int main()
+{
+	printf("%d\n", max(10, 20));
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
