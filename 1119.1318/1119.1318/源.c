4b806f2456696db62a6e800dc80ea6e3b306e773
Nabandon <1839304120@qq.com>
2019-12-05 11:37:06 +0800

    水仙花数

--- 1119.1318/1119.1318/源.c
+++ 1119.1318/1119.1318/源.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-int mian()
+void main()
 {
 	int i, j, k,num;
 	for (i = 1; i < 10; i++)
-	
+	{
 		for (j = 0; j < 10; j++)
-		
-			for (k = 0; k < 10; k++)
+	      for (k = 0; k < 10; k++)
+	{
+		num = 100 * i + 10 * j + k;
+		if (num == (i*i*i + j*j*j + k*k*k))
 
-				num = 100 * i + 10 * j + k;
-		if (num = i ^ 3 + j ^ 3 + k ^ 3)
-			printf("%d", num);
-	system("pause");
+			printf("%d, ", num);
+	}
+	}system("pause");
 	return 0;
 }
