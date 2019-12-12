6806f83e159834779375a84ba1f42a14ac47ad8c
Nabandon <1839304120@qq.com>
2019-12-12 18:09:27 +0800

    斐波那契数

---
+++ Project89/Project89/源.cpp
@@ -0,0 +1,14 @@
+//ì³²¨ÄÇÆõÊý
+#include<stdio.h>
+#include <stdlib.h>
+
+int feib(int n){
+	
+	
+	if (n <= 2)
+		return 1;
+	return feib(n - 1) + feib(n - 2);
+}void main(){
+	feib(8);
+	printf("%d", feib(8));
+}
